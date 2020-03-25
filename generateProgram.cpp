079e42d8548a1315faac7b8f2bdc8053646c852b
Nblaaa <nblaaa@mail.ru>
2020-03-25 22:25:37 +0700

    CPlusPlus factory has been added to generateProgram.cpp

--- generateProgram.cpp
+++ generateProgram.cpp
@@ -1,16 +1,16 @@
-#include "ClassUnit.h"
+/*#include "ClassUnit.h"
 #include "MethodUnit.h"
-#include "PrintOperatorUnit.h"
-
-const std::vector< std::string > ClassUnit::ACCESS_MODIFIERS = {"public", "protected", "private"};
+#include "PrintOperatorUnit.h"*/
+#include "CPlusPlusClasses/CPlusPlusFactory.h"
 
 std::string generateProgram() {
-    ClassUnit myClass( "MyClass" );
-    myClass.add(std::make_shared< MethodUnit >( "testFunc1", "void", 0 ), ClassUnit::PUBLIC);
-    myClass.add(std::make_shared< MethodUnit >( "testFunc2", "void", MethodUnit::STATIC ), ClassUnit::PRIVATE);
-    myClass.add(std::make_shared< MethodUnit >( "testFunc3", "void", MethodUnit::VIRTUAL | MethodUnit::CONST ), ClassUnit::PUBLIC);
-    auto method = std::make_shared< MethodUnit >( "testFunc4", "void", MethodUnit::STATIC);
-    method->add( std::make_shared< PrintOperatorUnit >( R"(Hello, world!\n)" ) );
-    myClass.add( method, ClassUnit::PROTECTED );
-    return myClass.compile();
+    AbstractFactory *factory = new CPlusPlusFactory();
+    ClassUnit *myClass = factory->CreateClassUnit("MyClass");
+    myClass->add(std::make_shared<MethodUnit>("testFunc1", "void", 0), ClassUnit::PUBLIC);
+    myClass->add(std::make_shared<MethodUnit>("testFunc2", "void", MethodUnit::STATIC), ClassUnit::PRIVATE);
+    myClass->add(std::make_shared<MethodUnit>("testFunc3", "void", MethodUnit::VIRTUAL | MethodUnit::CONST), ClassUnit::PUBLIC);
+    auto method = std::make_shared<MethodUnit>("testFunc4", "void", MethodUnit::STATIC);
+    method->add(std::make_shared<PrintOperatorUnit>(R"(Hello, world!\n)"));
+    myClass->add(method, ClassUnit::PROTECTED);
+    return myClass->compile();
 }
